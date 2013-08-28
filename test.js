5e853df3de479289b426ca9ea060fa3d96bf5260
Rob Britton <rob@sweetiq.com>
2013-08-28 11:10:02 -0400

    Linted tests. Bumped version number.

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
+/*jslint indent: 2, plusplus: true, sloppy: true */
+/*global require*/
 var randgen = require("./lib/randgen"),
-    assert = require('assert');
+  assert = require('assert');
 
-test_norms(1000, 5, 1);
-test_norms(1000, 500, 100);
-test_pois(1000, 5, 1);
-test_pois(1000, 500, 100);
-test_chisq(1000, 5, 1);
-test_chisq(1000, 500, 100);
+function sum(lhs, rhs) {
+  return lhs + rhs;
+}
+
+function mean(arr) {
+  var s = arr.reduce(sum, 0);
+  return s / arr.length;
+}
    
 function test_norms(n, mu, sigma) {
   var norms, xbar;
@@ -14,10 +18,10 @@ function test_norms(n, mu, sigma) {
   xbar = mean(norms);
 
   assert.equal(
-    Math.round(xbar/mu),
+    Math.round(xbar / mu),
     1,
-    "Generated normal population has xbar " + 
-    xbar +  " is not close to its expected value, " + mu + "."
+    "Generated normal population has xbar " +
+      xbar +  " is not close to its expected value, " + mu + "."
   );
 }
 
@@ -27,10 +31,10 @@ function test_pois(n, lambda) {
   xbar = mean(pois);
 
   assert.equal(
-    Math.round(xbar/lambda),
+    Math.round(xbar / lambda),
     1,
     "Poisson population mean " + xbar +
-    " is not close to the expected value " + lambda + "."
+      " is not close to the expected value " + lambda + "."
   );
 }
 
@@ -41,23 +45,24 @@ function test_chisq(n, k) {
   xbar = mean(chisq);
 
   assert.equal(
-    Math.round(xbar/k),
+    Math.round(xbar / k),
     1,
     "Chi-squared population mean " + xbar +
-    " is not close to expected value " + k + "."
+      " is not close to expected value " + k + "."
   );
 }
 
 function test_cauchy(n, loc, scale) {
   var cauchy = randgen.rvcauchy(1000);
   // it's complicated
+  // cauchy is difficult to test using means simply because the true mean for
+  // a cauchy distribution is undefined
+  // TODO: figure out a good way to test Cauchy numbers
 }
 
-function mean(arr) {
-  var s = arr.reduce(sum, 0);
-  return s / arr.length;
-}
-
-function sum(lhs, rhs) {
-  return lhs + rhs;
-}
+test_norms(1000, 5, 1);
+test_norms(1000, 500, 100);
+test_pois(1000, 5, 1);
+test_pois(1000, 500, 100);
+test_chisq(1000, 5, 1);
+test_chisq(1000, 500, 100);
